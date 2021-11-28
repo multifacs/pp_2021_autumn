f9a802cb4a441ac3552480facfa65fc106edfad2
Nick <Nick@DESKTOP-0H298T3>
2021-11-28 18:24:46 +0300

    fix?

--- modules/task_1/vorobyova_a_sleeping_barber/sleeping_barber.h
+++ modules/task_1/vorobyova_a_sleeping_barber/sleeping_barber.h
@@ -2,6 +2,8 @@
 #ifndef MODULES_TASK_1_VOROBYOVA_A_SLEEPING_BARBER_SLEEPING_BARBER_H_
 #define MODULES_TASK_1_VOROBYOVA_A_SLEEPING_BARBER_SLEEPING_BARBER_H_
 
+#include <memory>
+
 typedef struct Message_s {
   int command;
   int id_proc;
@@ -15,11 +17,22 @@ class Client {
  public:
   int place_in_line;
   explicit Client(int id_proc = -1, int id_line = 1, int id_barber = 0)
-      : id_proc(id_proc), id_line(id_line), id_barber(id_barber) {}
+      : id_proc(id_proc),
+        id_line(id_line), id_barber(id_barber) {
+    place_in_line = 0;
+  }
   int onHold();
   void initiate();
   void cut();
   int getId_Proc() { return id_proc; }
+  //~Client() {
+  //  if (this != nullptr) {
+  //    id_proc = 0;
+  //    id_line = 0;
+  //    id_barber = 0;
+  //    place_in_line = 0;
+  //  }
+  //}
 };
 class Line {
   Client *ClientLine;
@@ -52,6 +65,42 @@ class Line {
   }
   int check_line();
   void initiate();
+  // Copy constructor
+  Line(const Line &l1) {
+    this->id_proc = l1.id_proc;
+    this->length = l1.length;
+    this->id_barber = l1.id_barber;
+    this->num_proc_exec = l1.num_proc_exec;
+    ClientLine = new Client[length];
+    clients_num = 0;
+    max_clients = length / 2;
+    barber_sleeping = false;
+  }
+  Line& operator=(const Line& other) {
+    // Guard self assignment
+    if (this == &other) return *this;
+    // assume *this manages a reusable resource, such as a heap-allocated buffer
+    // mArray
+    if (this->length != other.length) {  // resource in *this cannot be reused
+      delete[] ClientLine;         // release resource in *this
+      this->ClientLine = nullptr;
+      this->length = 0;  // preserve invariants in case next line throws
+      this->ClientLine =
+          new Client[other.length];  // allocate resource in *this
+      this->length = other.length;
+    }
+    std::copy(other.ClientLine, other.ClientLine + other.length, ClientLine);
+
+    this->id_proc = other.id_proc;
+    this->id_barber = other.id_barber;
+    this->num_proc_exec = other.num_proc_exec;
+    clients_num = 0;
+    max_clients = length / 2;
+    barber_sleeping = false;
+
+    return *this;
+  }
+  ~Line() = default;
 };
 class Barber {
   int id_proc;
