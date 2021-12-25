4922708ea69d9f5b3290a0935b6f75c398401b4c
Nick <Nick@DESKTOP-0H298T3>
2021-12-25 14:53:17 +0300

    bar

---
+++ modules/task_3/barysheva_m_radix_batcher/main.cpp
@@ -0,0 +1,212 @@
+// Copyright 2021 Barysheva Maria
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <gtest-mpi-listener.hpp>
+#include <vector>
+
+#include "./radix_batcher.h"
+
+TEST(RADIX_BATCHER, TEST_1) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (log2(size) == static_cast<int>(log2(size))) {
+    double *vec, *vec_p, *vec_s;
+    double start, end;
+    int size_v = pow(2, 4);
+    if (rank == 0) {
+      std::cout << "Size: " << size_v << std::endl;
+      vec = getRandomVector(size_v);
+      vec_p = new double[size_v];
+      vec_s = new double[size_v];
+      for (int i = 0; i < size_v; i++) {
+        vec_p[i] = vec[i];
+        vec_s[i] = vec[i];
+      }
+    }
+    start = MPI_Wtime();
+    RadixSortParallel(&vec_p, size_v);
+    end = MPI_Wtime();
+    if (rank == 0) {
+      double ptime = end - start;
+      start = MPI_Wtime();
+      floatRadixSort<double>(&vec_s, size_v);
+      end = MPI_Wtime();
+      double stime = end - start;
+      for (int i = 0; i < size_v; i++) {
+        EXPECT_DOUBLE_EQ(vec_p[i], vec_s[i]);
+        // std::cout << vec[i] << " " << vec_s[i] << std::endl;
+      }
+      std::cout << "Parallel: " << ptime << " Sequential: " << stime
+                << std::endl;
+      std::cout << "Speedup: " << stime / ptime << std::endl;
+    }
+  }
+}
+
+TEST(RADIX_BATCHER, TEST_2) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (log2(size) == static_cast<int>(log2(size))) {
+    double *vec, *vec_p, *vec_s;
+    double start, end;
+    int size_v = pow(2, 5);
+    if (rank == 0) {
+      std::cout << "Size: " << size_v << std::endl;
+      vec = getRandomVector(size_v);
+      vec_p = new double[size_v];
+      vec_s = new double[size_v];
+      for (int i = 0; i < size_v; i++) {
+        vec_p[i] = vec[i];
+        vec_s[i] = vec[i];
+      }
+    }
+    start = MPI_Wtime();
+    RadixSortParallel(&vec_p, size_v);
+    end = MPI_Wtime();
+    if (rank == 0) {
+      double ptime = end - start;
+      start = MPI_Wtime();
+      floatRadixSort<double>(&vec_s, size_v);
+      end = MPI_Wtime();
+      double stime = end - start;
+      for (int i = 0; i < size_v; i++) {
+        EXPECT_DOUBLE_EQ(vec_p[i], vec_s[i]);
+        // std::cout << vec[i] << " " << vec_s[i] << std::endl;
+      }
+      std::cout << "Parallel: " << ptime << " Sequential: " << stime
+                << std::endl;
+      std::cout << "Speedup: " << stime / ptime << std::endl;
+    }
+  }
+}
+
+TEST(RADIX_BATCHER, TEST_3) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (log2(size) == static_cast<int>(log2(size))) {
+    double *vec, *vec_p, *vec_s;
+    double start, end;
+    int size_v = pow(2, 10);
+    if (rank == 0) {
+      std::cout << "Size: " << size_v << std::endl;
+      vec = getRandomVector(size_v);
+      vec_p = new double[size_v];
+      vec_s = new double[size_v];
+      for (int i = 0; i < size_v; i++) {
+        vec_p[i] = vec[i];
+        vec_s[i] = vec[i];
+      }
+    }
+    start = MPI_Wtime();
+    RadixSortParallel(&vec_p, size_v);
+    end = MPI_Wtime();
+    if (rank == 0) {
+      double ptime = end - start;
+      start = MPI_Wtime();
+      floatRadixSort<double>(&vec_s, size_v);
+      end = MPI_Wtime();
+      double stime = end - start;
+      for (int i = 0; i < size_v; i++) {
+        EXPECT_DOUBLE_EQ(vec_p[i], vec_s[i]);
+        // std::cout << vec[i] << " " << vec_s[i] << std::endl;
+      }
+      std::cout << "Parallel: " << ptime << " Sequential: " << stime
+                << std::endl;
+      std::cout << "Speedup: " << stime / ptime << std::endl;
+    }
+  }
+}
+
+TEST(RADIX_BATCHER, TEST_4) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (log2(size) == static_cast<int>(log2(size))) {
+    double *vec, *vec_p, *vec_s;
+    double start, end;
+    int size_v = pow(2, 11);
+    if (rank == 0) {
+      std::cout << "Size: " << size_v << std::endl;
+      vec = getRandomVector(size_v);
+      vec_p = new double[size_v];
+      vec_s = new double[size_v];
+      for (int i = 0; i < size_v; i++) {
+        vec_p[i] = vec[i];
+        vec_s[i] = vec[i];
+      }
+    }
+    start = MPI_Wtime();
+    RadixSortParallel(&vec_p, size_v);
+    end = MPI_Wtime();
+    if (rank == 0) {
+      double ptime = end - start;
+      start = MPI_Wtime();
+      floatRadixSort<double>(&vec_s, size_v);
+      end = MPI_Wtime();
+      double stime = end - start;
+      for (int i = 0; i < size_v; i++) {
+        EXPECT_DOUBLE_EQ(vec_p[i], vec_s[i]);
+      }
+      std::cout << "Parallel: " << ptime << " Sequential: " << stime
+                << std::endl;
+      std::cout << "Speedup: " << stime / ptime << std::endl;
+    }
+  }
+}
+
+TEST(RADIX_BATCHER, TEST_5) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (log2(size) == static_cast<int>(log2(size))) {
+    double *vec, *vec_p, *vec_s;
+    double start, end;
+    int size_v = pow(2, 12);
+    if (rank == 0) {
+      std::cout << "Size: " << size_v << std::endl;
+      vec = getRandomVector(size_v);
+      vec_p = new double[size_v];
+      vec_s = new double[size_v];
+      for (int i = 0; i < size_v; i++) {
+        vec_p[i] = vec[i];
+        vec_s[i] = vec[i];
+      }
+    }
+    start = MPI_Wtime();
+    RadixSortParallel(&vec_p, size_v);
+    end = MPI_Wtime();
+    if (rank == 0) {
+      double ptime = end - start;
+      start = MPI_Wtime();
+      floatRadixSort<double>(&vec_s, size_v);
+      end = MPI_Wtime();
+      double stime = end - start;
+      for (int i = 0; i < size_v; i++) {
+        EXPECT_DOUBLE_EQ(vec_p[i], vec_s[i]);
+        // std::cout << vec[i] << " " << vec_s[i] << std::endl;
+      }
+      std::cout << "Parallel: " << ptime << " Sequential: " << stime
+                << std::endl;
+      std::cout << "Speedup: " << stime / ptime << std::endl;
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
+
+  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+  ::testing::TestEventListeners &listeners =
+      ::testing::UnitTest::GetInstance()->listeners();
+
+  listeners.Release(listeners.default_result_printer());
+  listeners.Release(listeners.default_xml_generator());
+
+  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+  return RUN_ALL_TESTS();
+}
---
+++ modules/task_3/barysheva_m_radix_batcher/radix_batcher.cpp
@@ -0,0 +1,103 @@
+// Copyright 2021 Barysheva Maria
+#include "../../../modules/task_3/barysheva_m_radix_batcher/radix_batcher.h"
+
+#include <mpi.h>
+
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <random>
+#include <utility>
+#include <vector>
+
+double* getRandomVector(const int size) {
+  std::mt19937 gen(time(0));
+  std::uniform_real_distribution<> urd(0, 300);
+  double* vec = new double[size];
+  for (int i = 0; i < size; i++) vec[i] = urd(gen);
+
+  return vec;
+}
+
+double* merge(double* vec1, double* vec2, int size1, int size2) {
+  int vec1_elem = 0, vec2_elem = 0, k = 0;
+  double* result = new double[size1 + size2];
+
+  while (vec1_elem < size1 && vec2_elem < size2)
+    if (vec1[vec1_elem] < vec2[vec2_elem]) {
+      result[k] = vec1[vec1_elem];
+      vec1_elem++;
+      k++;
+    } else {
+      result[k] = vec2[vec2_elem];
+      vec2_elem++;
+      k++;
+    }
+  if (vec1_elem == size1) {
+    while (vec2_elem < size2) {
+      result[k] = vec2[vec2_elem];
+      vec2_elem++;
+      k++;
+    }
+  } else {
+    while (vec1_elem < size1) {
+      result[k] = vec1[vec1_elem];
+      vec1_elem++;
+      k++;
+    }
+  }
+  return result;
+}
+
+void RadixSortParallel(double** arr, int size_v) {
+  double* chunk;
+  double* other;
+  int m = size_v, n = size_v;
+  int rank, size;
+  int delta, i, step = 1;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (rank == 0) {
+    int r;
+    delta = n / size;
+
+    MPI_Bcast(&delta, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    chunk = new double[delta];
+    MPI_Scatter(*arr, delta, MPI_DOUBLE, chunk, delta, MPI_DOUBLE, 0,
+                MPI_COMM_WORLD);
+    floatRadixSort<double>(&chunk, delta);
+  } else {
+    MPI_Bcast(&delta, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    chunk = new double[delta];
+    MPI_Scatter(*arr, delta, MPI_DOUBLE, chunk, delta, MPI_DOUBLE, 0,
+                MPI_COMM_WORLD);
+    floatRadixSort<double>(&chunk, delta);
+  }
+
+  while (step < size) {
+    int current_rank = rank % (2 * step);
+    if (current_rank == 0) {
+      if (rank + step < size) {
+        MPI_Recv(&m, 1, MPI_INT, rank + step, 0, MPI_COMM_WORLD,
+                 MPI_STATUSES_IGNORE);
+        other = new double[m];
+        MPI_Recv(other, m, MPI_DOUBLE, rank + step, 0, MPI_COMM_WORLD,
+                 MPI_STATUSES_IGNORE);
+        chunk = merge(chunk, other, delta, m);
+        delta = delta + m;
+        fflush(stdout);
+      }
+    } else {
+      int near = rank - step;
+      MPI_Send(&delta, 1, MPI_INT, near, 0, MPI_COMM_WORLD);
+      MPI_Send(chunk, delta, MPI_DOUBLE, near, 0, MPI_COMM_WORLD);
+      break;
+    }
+    step = step * 2;
+  }
+  if (rank == 0) {
+    *arr = chunk;
+  }
+}
